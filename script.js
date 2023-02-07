70a3edd0f8cb0335ebcdc2286c9ec6a881836673
vcatic <vanesa.cat09@gmail.com>
2023-02-07 12:15:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// More info about initialization & config:
+			// - https://revealjs.com/initialization/
+			// - https://revealjs.com/config/
+			Reveal.initialize({
+				hash: true,
+
+				// Learn about plugins: https://revealjs.com/plugins/
+				plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+			});
